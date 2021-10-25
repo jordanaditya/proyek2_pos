b39216f9f25dc3fc1a976d6b538417aba2162a1d
Firmanrachmad <dragobniel@gmail.com>
2021-10-25 14:39:29 +0700

    update Tampilan Laporan

--- resources/views/laporan/laporan.blade.php
+++ resources/views/laporan/laporan.blade.php
@@ -1,15 +1,18 @@
 @extends('layouts.app')
-
+<!-- © 2020 Copyright: Tahu Coding -->
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Laporan') }}</div>
+    <div class="row">
+        <div class="col">
+            <div class="card" style="min-height: 85vh">
+                <div class="card-header bg-white"><h4 class="font-weight-bold">Laporan</h4></div>
                 <div class="card-body">
+                    
+                    <div></div>
                 </div>
             </div>
-        </div>
     </div>
 </div>
+</div>
+    
 @endsection
